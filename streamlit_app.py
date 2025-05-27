ec3409781b002a48648bdb72b5e347b3d39e0354
nafisakahla27 <nafisaaakahlanizarr@gmail.com>
2025-05-27 09:33:30 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,3 +5,14 @@ st.write(
     "ngodingseru berasama Bapak Hendri Setiadi"
 )
 st.image("IMG-20250526-WA0142.jpg", width=200)
+
+st.image("niku.jpg", width=200)
+
+st.title("Aplikasi Sederhana")
+st.header("Aplikasi Mengecek Nilai Genap/Ganjil")
+angka st.number_input("Tulis sebuah Angka:", value=0, step=1)
+
+if (angka % 2) 0:
+st.write(f"(angka) adalah Bilangan Genap")
+else:
+st.write(f"(angka) adalah Bilangan Ganjil")
